d7bd562420f0ee980d872ef823e3b5b86945cae9
Shamim Rahman <sda.hosain2@gmail.com>
2022-07-18 17:45:00 +0600

    code

---
+++ js/custom.js
@@ -0,0 +1,167 @@
+
+
+// let MyName = "Abdullah Al Shamim. ";
+//   let MyAge = 24;
+//   document.write("<br><br> The TypeOf Result is = " + typeof MyName + "<br>");
+//   MyName = "Shamim Rahman";
+//   document.write("<br> The TypeOf Result is = " + typeof MyAge + "<br><br><br>");
+//   const MyProfession = "Javascript Developer";
+//   const WantsToBe = "Front-end Web Developer";
+
+//   document.write("Hey there I'm " + MyName + ". I'm " + MyAge + " years old in 2021. I wants to be a " + MyProfession + ". Bascially my first goal is to be a " + WantsToBe + ".");
+
+
+// // fuction without return and simple.
+
+//   function firstFunction(parameterOne, paramterTwo, parameterThree){
+
+//     document.write("<br><br> First Function Result is = " + (parameterOne + paramterTwo - parameterThree) + "<br><br>");
+
+//   }
+
+//   firstFunction(2, 20, 300);
+
+
+//   // fuction with return.
+
+
+//   function secondFunction(parameterOne, paramterTwo, parameterThree){
+
+
+//     // return(parameterOne + paramterTwo - parameterThree);
+
+//     let retValue = (parameterOne + parameterThree * paramterTwo);
+//     return retValue;
+
+
+//   }
+
+//   let funResult = secondFunction(20, 20, 300);
+
+//   document.write(" Second Function Result is = " + funResult + "<br><br><br>");
+
+
+// // plus or addition function
+
+// function addition(num1 , num2){
+
+//     let addResult = (num1 + num2);
+//     document.write("Sum of addition is = " + addResult + "<br><br>");
+//     return addResult; 
+
+// }
+
+// // minus or substraction function
+
+// function substra(num1 , num2){
+
+//     let substraResult = (num1 - num2);
+//     document.write("Sum of substraction is = " + substraResult + "<br><br>");
+//     return substraResult;
+
+// }
+
+// // multipulation function
+
+// function multi (num1 , num2){
+
+//     let multiResult = (num1 * num2);
+//     document.write("Sum of multipulation is = " + multiResult + "<br><br>");
+//     return multiResult;
+
+// }
+
+// // division function
+
+// function divi (num1 , num2){
+
+//     let diviResult = (num1 / num2);
+//     document.write("Sum of division is = " + diviResult + "<br><br>");
+//     return diviResult;
+
+// }
+
+// // modulus function
+
+// function modu (num1 , num2){
+
+//     let moduResult = (num1 % num2);
+//     document.write("Sum of modulus is = " + moduResult + "<br><br>");
+//     return moduResult;
+
+// }
+
+// // explotion function
+
+// function explo (num1 , num2){
+
+//     let exploResult = (num1 ** num2);
+//     document.write("Sum of explotion is = " + exploResult + "<br><br>");
+//     return exploResult;
+
+// }
+
+// // increment function
+
+// function incre (num1){
+
+//     let increResult = (++ num1);
+//     document.write("Sum of increment is = " + increResult + "<br><br>");
+//     return increResult;
+
+// }
+
+// // decrement function
+
+// function decre (num1){
+
+//     let decreResult = (-- num1);
+//     document.write("Sum of decrement is = " + decreResult);
+//     return decreResult;
+
+// }
+
+
+// addition(300 , 400);
+// substra(88 , 39);
+// multi(30 , 58);
+// divi(400 , 28);
+// modu(400 , 28);
+// explo(3 , 5);
+// incre(6);
+// decre(0);
+
+
+// // basic Object
+
+
+// let mySchool = {
+
+//   name : "Pulia High School",
+//   class : "6 - 10",
+//   students : 6000,
+//   teachers : 20
+
+// }
+
+// document.write("<br> <br> My School Name is = " + mySchool.name + "<br>");
+// document.write(" My School Clases are = " + mySchool.class + "<br>");
+// document.write(" My School Students are = " + mySchool.students + "<br>");
+// document.write(" My School Teachers are = " + mySchool.teachers + "<br>");
+
+
+// // Built in Math Functions in JavaScript
+
+
+// document.write("<br>The value of Math.E is = " + Math.E + "<br>");
+// document.write("The value of Math.PI is = " + Math.PI + "<br>");
+// document.write(Math.abs(7.897) + "<br>");
+// document.write(Math.floor(7.897) + "<br>");
+// document.write(Math.ceil(7.297) + "<br>");
+// document.write(Math.round(7.697) + "<br>");
+// document.write(Math.max(87, 83, 75, 92, 2096) + "<br>");
+// document.write(Math.min(87, 83, 75, 92, 2096) + "<br>");
+// document.write(Math.round(Math.random() * 20 ) + "<br>");
+
+
+// document.write(new Date() + "<br>");
